ce4a79cd318cc3fbba303e389f80b97f6a43020d
Anastacio Ruiz <anastacior21@gmail.com>
2022-12-17 14:26:21 -0800

    skeleton

---
+++ assets/scraper.py
@@ -0,0 +1,19 @@
+url = 'https://twitter.com/ChipotleTweets'
+
+
+
+class Shovel:
+
+    def __init__(self):
+        self.run()
+
+    def newPost(self):
+        pass
+    def monitor(self):
+        pass
+
+    def checkPost(self):
+        pass
+
+    def go(self):
+         return # embed
